46274b21ad045a9a1c7faf5bfdef707bd8917577
Chenxiao223 <1215544202@qq.com>
2018-03-30 18:09:02 +0800

    0330

---
+++ app/src/main/java/com/zhiziyun/dmptest/bot/entity/CallInfo.java
@@ -0,0 +1,42 @@
+package com.zhiziyun.dmptest.bot.entity;
+
+/**
+ * Created by Administrator on 2018/3/29.
+ */
+
+public class CallInfo {
+
+    /**
+     * obj : A2168X10X0283440054-11-0-FLCS-GXI
+     * success : true
+     * msg : 13241690052
+     */
+
+    private String obj;
+    private boolean success;
+    private String msg;
+
+    public String getObj() {
+        return obj;
+    }
+
+    public void setObj(String obj) {
+        this.obj = obj;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
